2c965d9d96284c17009365bd9cc5efdbd234c231
Jori Regter <joriregter@gmail.com>
2021-10-04 09:29:53 +0200

    chore: small fixes and improvements to menus

--- manual/manual_toggler.gd
+++ manual/manual_toggler.gd
@@ -4,6 +4,7 @@ export (Resource) var manual_resource
 onready var manual: Manual = manual_resource
 
 func _ready():
+	self.hide()
 	manual.connect("manual_opened", self, "_on_manual_opened")
 
 func _on_manual_opened():
@@ -11,5 +12,11 @@ func _on_manual_opened():
 
 func _input(event):
 	if InputHelper.is_close_manual_just_pressed():
-		manual.close_manual()
-		self.hide()
+		close()
+	
+func _on_Escape_pressed():
+	close()
+
+func close():
+	manual.close_manual()
+	self.hide()
