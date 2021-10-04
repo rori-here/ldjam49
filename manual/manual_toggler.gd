d3fc626eb66feb5688d5bdbf6702bad1c89b033f
Robin Heij <rooobiin89@gmail.com>
2021-10-04 20:59:08 +0200

    feat: add audio

--- manual/manual_toggler.gd
+++ manual/manual_toggler.gd
@@ -3,12 +3,15 @@ extends Control
 export (Resource) var manual_resource
 onready var manual: Manual = manual_resource
 
+onready var audioListener = $Audio
+
 func _ready():
 	self.hide()
 	manual.connect("manual_opened", self, "_on_manual_opened")
 
 func _on_manual_opened():
 	self.show()
+	audioListener.play(0.0)
 
 func _input(event):
 	if InputHelper.is_close_manual_just_pressed():
@@ -19,4 +22,5 @@ func _on_Escape_pressed():
 
 func close():
 	manual.close_manual()
+	audioListener.play(0.0)	
 	self.hide()
