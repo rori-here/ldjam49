c128e3262949c156f56b73828559fdea9da6710a
Jori Regter <joriregter@gmail.com>
2021-10-04 14:54:59 +0200

    adds timer sprites

--- levels/day_ui.gd
+++ levels/day_ui.gd
@@ -3,8 +3,8 @@ extends CenterContainer
 export (Resource) var day_resource
 onready var day: Day = day_resource
 
-onready var day_label = $DayLabel
-onready var day_label_initial_text = $DayLabel.text
+onready var day_label = $Panel/DayLabel
+onready var day_label_initial_text = day_label.text
 
 func _ready():
 	set_text(day.get_time())
